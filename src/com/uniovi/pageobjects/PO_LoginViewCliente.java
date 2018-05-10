5ea622fb64148593214d818a084db1d68c0909ec
DESKTOP-V9D9FB7\Pineirin <pineirin@gmail.com>
2018-05-10 18:01:04 +0200

    mejores test

---
+++ src/com/uniovi/pageobjects/PO_LoginViewCliente.java
@@ -0,0 +1,30 @@
+package com.uniovi.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PO_LoginViewCliente extends PO_NavView {
+	
+	static public void fillForm(WebDriver driver, String usernamep, String passwordp) {
+		
+			WebElement username = driver.findElement(By.name("email"));
+			username.click();
+			username.clear();
+			username.sendKeys(usernamep);
+
+			WebElement password = driver.findElement(By.name("password"));
+			password.click();
+			password.clear();
+			password.sendKeys(passwordp);
+			
+			//Pulsar el boton de Alta.
+			By boton = By.className("btn");
+			driver.findElement(boton).click();
+		}
+
+	static public void esperaCargaForm(WebDriver driver) {
+		checkKey(driver, "login.message", PO_Properties.getSPANISH());
+	}
+}
+
